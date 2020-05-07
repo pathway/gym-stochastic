63a9d22a570f344b0551f764d1b60ddc454ab122
Robin Ranjit Singh Chauhan <robin@pathwayi.com>
2020-05-06 22:52:17 -0700

    notebook

--- gym_stochastic/envs/__init__.py
+++ gym_stochastic/envs/__init__.py
@@ -1,4 +1,4 @@
 from gym_stochastic.envs.wetchicken1d import WetChicken1dEnv
-from gym_stochastic.envs.bandits import DistributionContextualBanditEnv
+from gym_stochastic.envs.distbandits import DistributionContextualBanditEnv
 from gym_stochastic.envs.dist_utils import *
 
